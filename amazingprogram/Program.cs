63d2f518808efe7dae9ae73b9921546ca4e83a2a
Erni <eresv6@gmail.com>
2017-01-12 10:39:48 +0100

    Add files via upload

---
+++ amazingprogram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace amazingprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Wprowadz pierwsza liczbe");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wprowadz druga liczbe");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wynik = " + (a+b));
+            Console.ReadKey();
+        }
+    }
+}
