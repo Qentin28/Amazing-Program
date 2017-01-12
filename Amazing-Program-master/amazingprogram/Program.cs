2ecccad1a4d2b54ac4b0b86c4c3bd951381aabeb
Qentin28 <miliankolp@wp.pl>
2017-01-12 11:07:58 +0100

    Add files via upload

---
+++ Amazing-Program-master/amazingprogram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace amazingprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, c;
+            Console.WriteLine("Wprowadz pierwsza liczbe");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wprowadz druga liczbe");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wyniksxxx = " + (a+c));
+            Console.ReadKey();
+        }
+    }
+}
